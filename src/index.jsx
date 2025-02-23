225ec009eaf967ce6a3d0ceb41e850a7566f15e0
Kat <avchimchik@gmail.com>
2025-02-23 18:33:26 +0500

    Запрос на сервер Марвел идет и данные приходят

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "@/components/app/App.jsx";
+import MarvelService from "./services/MarvelService";
 import "@/style/style.scss";
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => console.log(res));
 
 const container = document.getElementById("root");
 const root = createRoot(container);
