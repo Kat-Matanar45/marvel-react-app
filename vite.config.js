4e04b8f46300ba63fa39aa55405616e2e5c387e3
Kat <avchimchik@gmail.com>
2025-03-15 18:44:59 +0500

    Изменены настройки конфига base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: "/marvel-react-app"
+  base: "/marvel-react-app",
   plugins: [react()],
   resolve: {
     alias: {
